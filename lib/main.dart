cb90659f9bbe0ae581dbecab5a6d7e68f8440769
Victor Alves De Carvalho <victor.a.carvalho97@gmail.com>
2022-05-05 13:08:42 -0300

    pre-definição do theme do app no main para futuras mudanças

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,36 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:projeto_final_unb/telas/Home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    localizationsDelegates: [
-      GlobalMaterialLocalizations.delegate,
-      GlobalWidgetsLocalizations.delegate,
-    ],
-    supportedLocales: const [Locale('pt', 'BR')],
-    home: Home(),
-    debugShowCheckedModeBanner: false,
-    //theme: basicTheme(),
-  ));
+  runApp(
+    MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('pt', 'BR')],
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+      //theme:
+    ),
+  );
+}
+
+//ESTUDAR UMA PALETA DE CORES PRO APP FUTURAMENTE E MUDAR UM POUCO O DESIGN DELE
+ThemeData _appTheme() {
+  return ThemeData(
+    // Define the default brightness and colors.
+    brightness: Brightness.dark,
+    primaryColor: Colors.lightBlue[800],
+
+    // Define the default font family.
+    fontFamily: 'Georgia',
+
+    // Define the default `TextTheme`. Use this to specify the default
+    // text styling for headlines, titles, bodies of text, and more.
+    textTheme: const TextTheme(
+      headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+      headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+      bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+    ),
+  );
 }
